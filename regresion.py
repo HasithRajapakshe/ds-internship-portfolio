314d08aad4c454337fe3785d372faa0a4ddac855
Hasith Rajapakashe <hasithharindarajapakshe@gmail.com>
2026-03-24 15:08:24 +0530

    fix error changing MSE into RMSE

--- regresion.py
+++ regresion.py
@@ -1,21 +1,26 @@
-import numpy as np
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 import joblib
+import numpy as np
 
-data = pd.read_csv('Housing.csv')
+
+data = pd.read_csv(
+    r'C:\Users\hasit\OneDrive\Desktop\ds_internship_portfolio\ds-internship\Task3\Housing.csv')
 print(data)
+
 
 plt.scatter(data['area'], data['price'], color='red')
 plt.xlabel('area')
 plt.ylabel('price')
 plt.show()
 
-x = np.array(data['area']).reshape(-1, 1)
-y = np.array(data['price'])
+df = pd.DataFrame(data)
+x = df['area'].values.reshape(-1, 1)
+y = df['price'].values.reshape(-1, 1)
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, test_size=0.2, random_state=0)
@@ -33,8 +38,8 @@ plt.show()
 
 loaded_model = joblib.load('housing_model.pkl')
 y_pred = loaded_model.predict(x_test)
-mean = mean_squared_error(y_test, y_pred)
-print("mean:", mean)
+rmse1 = np.sqrt(mean_squared_error(y_test, y_pred))
+print("RMSE:", rmse1)
 
 print("intercept:", model.intercept_)
 print("coef:", model.coef_)
